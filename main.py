65d01950d366c930ee72a389764a5fc175a939f9
Sirri69 <pranav2278@gmail.com>
2021-07-16 10:59:16 +0530

    Finished work

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
-import hmac
+from flask.cli import with_appcontext
+
 import hashlib
+import datetime
+import click
+
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import sql
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -9,9 +14,12 @@ from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
 
+# NOTE:- request.json (instead of request.form) is used in post requests. Please write json body in postman for post requests to work 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretstuff'
 app.config['JWT_SECRET_KEY'] = 'supersecretstuff'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 jwt = JWTManager(app)
 
@@ -37,24 +45,22 @@ class Tasks(db.Model):
     worker = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
 
 
+@click.command('create-tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
+
 def authenticate(uname, passw):
     user = Users.query.filter_by(username=uname).first() or None
     print('COMES HERE')
-    if user and hmac.compare_digest(user.password.encode('utf-8'), passw.encode('utf-8')):
+    if user and user.password == passw:
         return user
-
     return None
-
-
-def identity(payload):
-    print('COMES HERE')
-    u_id = payload['identity']
-    return Users.query.filter_by(id=u_id).first()
 
 @app.route('/login', methods=['POST'])
 def login():
     username = request.json.get("username", None)
-    password = request.json.get("password", None)
+    password = hashlib.md5(request.json.get("password", None).encode()).hexdigest()
     user = authenticate(username,password)
     # if username != "test" or password != "test":
     if user is None:
@@ -62,6 +68,128 @@ def login():
 
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token)
+
+
+@app.route('/signup-admin', methods=['POST'])
+def signup_admin():
+    username = request.json.get("username", None)
+    password = hashlib.md5(request.json.get("password", None).encode()).hexdigest()
+    role = 'admin'
+    u = Users(username=username, password=password, role=role)
+    db.session.add(u)
+    db.session.commit()
+    return 'Admin added !'
+
+
+@app.route('/signup-worker', methods=['POST'])
+def signup_worker():
+    username = request.json.get("username", None)
+    password = hashlib.md5(request.json.get("password", None).encode()).hexdigest()
+    role = 'worker'
+    u = Users(username=username, password=password, role=role)
+    db.session.add(u)
+    db.session.commit()
+    return 'User added !'
+
+
+@app.route('/users')
+@jwt_required()
+def users():
+    j = {}
+    for i in Users.query.all():
+        j[i.username] = {
+            'role': i.role,
+            'tasks': len(i.task)
+        }
+    
+    return jsonify(j)
+
+
+@app.route('/jobs', methods=['GET', 'POST'])
+@jwt_required()
+def jobs():
+    if request.method == 'GET':
+        current_user = Users.query.filter_by(username=get_jwt_identity()).first() or None
+        if current_user.role == 'admin':
+            worker =  Users.query.filter_by(username=request.args.get('worker')).first() or None 
+        else:
+            worker = current_user
+        user = worker # Users.query.filter_by(username=worker).first() or None
+        j = {}
+        if user:
+            for i in user.task:
+                j[i.task] = i.status
+            return jsonify(j)
+        elif user is None:
+            return 'No such user'
+
+    elif request.method == 'POST':
+        worker  = request.json.get('worker_name') # worker name is used instead of worker id
+        task = request.json.get('task')
+        status = request.json.get('status')
+
+        user =  Users.query.filter_by(username=worker).first() or None
+        if user:
+            t = Tasks(task=task, status=status)
+            user.task.append(t)
+            db.session.add(user)
+            db.session.commit()
+
+            return 'Task added successfully !'
+        elif user is None:
+            return 'No such worker'
+
+
+@app.route('/update-job', methods=['POST'])
+@jwt_required()
+def update_job():
+    current_user = Users.query.filter_by(username=get_jwt_identity()).first() or None
+    if current_user.role == 'admin':
+        worker = Users.query.filter_by(username=request.json.get('worker')).first() or None
+        print(worker)
+    else:
+        worker = current_user
+    user = worker       
+
+    if user is None: return 'No such worker'
+
+    task = request.json.get('task')
+    status = request.json.get('status')
+    task = Tasks.query.filter_by(task=task, worker=user.id).first() or None
+
+    if task is None: return 'No such task'
+
+    task.status = status
+
+    db.session.add(task)
+    db.session.commit()
+
+    return 'Task updated'
+
+
+
+@app.route('/delete-job', methods=['POST'])
+@jwt_required()
+def delete_job():
+    current_user = Users.query.filter_by(username=get_jwt_identity()).first() or None
+    if current_user.role != 'admin': return 'You are not allowed to do this action'
+    worker = Users.query.filter_by(username=request.json.get('worker')).first() or None
+
+    if worker is None: return 'No such worker'
+
+    task = request.json.get('task')
+    task = Tasks.query.filter_by(task=task, worker=worker.id).first() or None
+
+    if task is None: return 'No such task'
+
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return 'Task deleted'
+
+
+
 
 @app.route("/protected", methods=["GET"])
 @jwt_required()
